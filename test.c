38ea1a3774832052117fd1897af85f9ca3e3f1b1
Dima <dima.k7356@gmail.com>
2023-08-19 13:47:10 +0300

    finish for today

--- test.c
+++ test.c
@@ -4,9 +4,11 @@
 int rtos_main()
 {
     printf("Hello world\r\n");
-    int x,y;
-    x=2.5;
-    y =3;
-    printf("%f\n",x+y);
+    int x, y;
+    x = 2.5;
+    y = 3;
+    printf("%f\n", x + y);
+    printf("Hello Dima\r\n");
+
     return 0;
 }
