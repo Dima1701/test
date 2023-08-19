fc16e51a9e9d47ddd22253b80f7833ae95afba1f
Dima <dima.k7356@gmail.com>
2023-08-19 13:50:59 +0300

    finish

--- test2.c
+++ test2.c
@@ -3,9 +3,10 @@
 void mymain()
 {
     printf("Hello from file 2\r\n");
-    printf("Hello Dims\r\n");
-    int x,y;
-    x= 10;
+    printf("Hello Dima\r\n");
+    int x, y;
+    x = 10;
     y = 5;
-    printf("%d\n",x+y);
+    printf("%d\n", x + y);
+    printf("Hello Dima again\r\n");
 }
