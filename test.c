8c83ca09b5da8897e8946a2b71d38db5601c8716
Dima <dima.k7356@gmail.com>
2023-08-19 13:29:34 +0300

    This is a new commit for what I originally planned to be amended

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdint.h"
+
+int rtos_main()
+{
+    printf("Hello world\r\n");
+    int x,y;
+    x=2.5;
+    y =3;
+    printf("%f\n",x+y);
+    return 0;
+}
---
+++ test2.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+void mymain()
+{
+    printf("Hello from file 2\r\n");
+    printf("Hello Dims\r\n");
+    int x,y;
+    x= 10;
+    y = 5;
+    printf("%d\n",x+y);
+}
